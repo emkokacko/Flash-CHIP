6a068ed18ea47899a17b6b84f08620c60828dff5
emkokacko <70253003+emkokacko@users.noreply.github.com>
2023-06-30 11:23:36 +0200

    Create Buster to Bullseye.sh

---
+++ CHIP-updater/Buster to Bullseye.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Upgrade from buster to bullseye
+echo "."
+echo "*** Updating and upgrading Debian Stretch to Buster. ***"
+mv /etc/apt/sources.list /etc/apt/sources.list.bak
+wget https://raw.githubusercontent.com/emkokacko/Flash-CHIP/master/CHIP-updater/buster_source_list.txt
+mv buster_source_list.txt /etc/apt/sources.list
+
+echo "."
+echo "*** apt update & upgrade ***"
+apt update
+apt install linux-image-armmp -y --force-yes
+apt full-upgrade -y --force-yes
+sleep 5
+
+
+# Define X11 variables for buster
+sudo mv /etc/X11/xorg.conf /etc/X11/xorg.conf.bak
+wget https://raw.githubusercontent.com/emkokacko/Flash-CHIP/master/CHIP-updater/buster_x11.txt
+mv buster_x11.txt /etc/X11/xorg.conf
+
+apt autoremove -y --force-yes
+echo "."
+echo "*** Update to Buster finished. Reboot***"
